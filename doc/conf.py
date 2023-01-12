6970326702e80a4b21e0ad4999200fff66bcfc9e
Andrew Crabb <crabba@amazon.com>
2023-01-12 13:06:23 -0500

    Base Sphinx-Rst project

--- doc/conf.py
+++ doc/conf.py
@@ -9,19 +9,18 @@
 project = 'EMR on Service Workbench'
 copyright = '2022, Andrew Crabb'
 author = 'Andrew Crabb'
+release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
+extensions = ['sphinx.ext.autodoc']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = 'sphinxdoc'
+html_theme_path = ['.']
+html_static_path = ['../_static']
---
+++ doc/index.rst
@@ -0,0 +1,14 @@
+########################
+EMR on Service Workbench
+########################
+
+Source code for this guide is hosted at `GitHub <https://github.com:crabba/emr-on-service-workbench>`_: Feedback and pull requests are welcomed.
+
+.. toctree::
+   :numbered:
+   :maxdepth: 2
+   :caption: Contents:
+
+   introduction.rst
+   service-workbench.rst
+   hail-on-amazon-emr.rst
